4cfaa44643cc2df6e15b026a0b204b8c884c4393
bvictorb <victory-boucher@hotmail.com>
2020-10-22 14:17:31 +0200

    split up the functions even more, added comments

--- static/index.js
+++ static/index.js
@@ -1,8 +1,8 @@
 const eyeColor = (data) => {
     const
-        eyeColors = getData(data, 'oogKleur')
+        eyeColors = getData(data, 'lievelingskleur') // Creates an array of colors that users filled in
         colorContainer = document.querySelector('.color-container')
-        eyeColorsEdited = eyeColors.map(str => str.replace(/[^A-Z0-9]/ig, '').toUpperCase())
+        eyeColorsEdited = eyeColors.map(str => str.replace(/[^A-Z0-9]/ig, '').toUpperCase()) // Removes all special characters, spaces etc. from the array and makes all letters caps
         colorTranslate = {
         'blauw': '0000FF',
         'lichtblauw': 'ADD8E6',
@@ -10,34 +10,38 @@ const eyeColor = (data) => {
         'groen': '008000',
         'rood': 'FF0000',
         'zwart': '000000',
-        'geel': 'FFFF00'
-        }
+        'geel': 'FFFF00',
+        'legergroen': '4B5320',
+        'staal': '808080',
+        'grijs': '666666'
+        } // Object for changing users their incorrect entries to HEX color codes
 
-    const createColorTiles = (color) => {
+    // Function that runs all functions to create the colored tiles
+    const colorTiles = (color) => {
         const
             colorBlock = document.createElement('div')
             colorText = document.createElement('p')
 
         setColor(color, colorBlock, colorText)
         checkContrast(color, colorText)
-        
-        colorContainer.appendChild(colorBlock)
-        colorBlock.appendChild(colorText)
     }
 
+    // Function that checks the color, matches it with key in object, or just uses the color given by the user
     const setColor = (color, colorBlock, colorText) => {
+        let newColor
+
         if (color.toLowerCase() in colorTranslate) {
-            color = color.toLowerCase()
-            const newColor = colorTranslate[color]
-            colorText.innerHTML = '#' + newColor
-            colorBlock.style.backgroundColor = '#' + newColor
+            newColor = colorTranslate[color.toLowerCase()]
             checkContrast(newColor, colorText)
+        } else if (color.length === 6) {
+            newColor = color
         } else {
-            colorText.innerHTML = '#' + color
-            colorBlock.style.backgroundColor = '#' + color
+            return
         }
+        createColorTiles(newColor, colorBlock, colorText)
     }
 
+    // Function that translates the HEX color code to RGB, and then to YIQ. We use YIQ to check if we need to change the text color to white for better contrast
     const checkContrast = (color, colorText) => {
         const
             r = parseInt(color.substr(0, 2), 16)
@@ -50,8 +54,17 @@ const eyeColor = (data) => {
         }
     }
 
+    // Function that finally creates all the colored tiles (puts them in the DOM)
+    const createColorTiles = (newColor, colorBlock, colorText) => {
+        colorText.innerHTML = '#' + newColor
+        colorBlock.style.backgroundColor = '#' + newColor
+        colorContainer.appendChild(colorBlock)
+        colorBlock.appendChild(colorText)
+    }
+
+    // This forEach method loops trough all the formatted colors in the array and runs the createColorTiles function passing trough the filled in color
     eyeColorsEdited.forEach((eyeColor) => {
-        createColorTiles(eyeColor)
+        colorTiles(eyeColor)
     })
 }
 
