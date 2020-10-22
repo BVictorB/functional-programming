4a4555d51c0cb1f582a6b7945311ed8d71150de1
bvictorb <victory-boucher@hotmail.com>
2020-10-22 13:38:12 +0200

    cleaned up the color tiles functions

--- static/index.js
+++ static/index.js
@@ -1,29 +1,52 @@
 const eyeColor = (data) => {
-    const eyeColors = getData(data, 'oogKleur')
-    const colorContainer = document.querySelector('.color-container')
-    const eyeColorsEdited = eyeColors.map(str => str.replace(/[^A-Z0-9]/ig, '').toUpperCase())
-
-    // Add recognition of words and change them to colors
-    const createColorTiles = (color) => {
-        const colorBlock = document.createElement('div');
-        const colorText = document.createElement('p')
-        if (checkContrast(color)) {
-            colorText.style.color = 'white'
+    const
+        eyeColors = getData(data, 'oogKleur')
+        colorContainer = document.querySelector('.color-container')
+        eyeColorsEdited = eyeColors.map(str => str.replace(/[^A-Z0-9]/ig, '').toUpperCase())
+        colorTranslate = {
+        'blauw': '0000FF',
+        'lichtblauw': 'ADD8E6',
+        'bruin': '8B4513',
+        'groen': '008000',
+        'rood': 'FF0000',
+        'zwart': '000000',
+        'geel': 'FFFF00'
         }
-        colorText.innerHTML = '#' + color
-        colorBlock.style.backgroundColor = '#' + color
+
+    const createColorTiles = (color) => {
+        const
+            colorBlock = document.createElement('div')
+            colorText = document.createElement('p')
+
+        setColor(color, colorBlock, colorText)
+        checkContrast(color, colorText)
+        
         colorContainer.appendChild(colorBlock)
         colorBlock.appendChild(colorText)
     }
 
-    const checkContrast = (color) => {
-        const r = parseInt(color.substr(0, 2), 16);
-        const g = parseInt(color.substr(2, 2), 16);
-        const b = parseInt(color.substr(4, 2), 16);
-        const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+    const setColor = (color, colorBlock, colorText) => {
+        if (color.toLowerCase() in colorTranslate) {
+            color = color.toLowerCase()
+            const newColor = colorTranslate[color]
+            colorText.innerHTML = '#' + newColor
+            colorBlock.style.backgroundColor = '#' + newColor
+            checkContrast(newColor, colorText)
+        } else {
+            colorText.innerHTML = '#' + color
+            colorBlock.style.backgroundColor = '#' + color
+        }
+    }
+
+    const checkContrast = (color, colorText) => {
+        const
+            r = parseInt(color.substr(0, 2), 16)
+            g = parseInt(color.substr(2, 2), 16)
+            b = parseInt(color.substr(4, 2), 16)
+            yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000
 
         if (yiq <= 128) {
-            return true
+            colorText.style.color = 'white'
         }
     }
 
@@ -32,13 +55,13 @@ const eyeColor = (data) => {
     })
 }
 
-const dobAmount = (data) => {
+const debtAmount = (data) => {
     const studentDebts = getData(data, 'hoogteStudieschuld')
 
-    const sortStudentDebts = studentDebts.reduce((obj, b) => {
-        obj[b] = ++obj[b] || 1;
-        return obj;
-    }, {});
+    const sortStudentDebts = studentDebts.reduce((obj, key) => {
+        obj[key] = (obj[key] || 0) + 1 // Or '++obj[key] || 1' both would work 
+        return obj
+    }, {})
 
     for (key in sortStudentDebts) {
         console.log(key + ': ' + sortStudentDebts[key])
@@ -51,7 +74,7 @@ const getData = (data, column) => {
 
 const useData = (data) => {
     eyeColor(data)
-    dobAmount(data)
+    debtAmount(data)
 }
 
 fetch('./data/survey-dataset.json')
